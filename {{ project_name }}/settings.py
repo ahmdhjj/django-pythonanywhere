e74bfd7038b0f2754d575f0d7bf23a3ea642b881
ahmdhjj <77078034+ahmdhjj@users.noreply.github.com>
2023-10-28 08:52:57 -0400

    Update  SECRET_KEY env var in settings.py

--- {{ project_name }}/settings.py
+++ {{ project_name }}/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5z$uu8ul=(@)+(29=q29ge1r#)zd%+0ye!16pxzl(g28iy-dun'
+# PythonAnywhere recommends using a .env file and `python-dotenv` to env vars
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
